6c9f3d66969b450c2922b9dfa9f28abded8e95dd
bosekarmegam <suneelbosekarmegam@gmail.com>
2026-03-03 01:38:34 +0530

    Revert "Initial commit ✨"
    This reverts commit 8fa1e0decca3e65d2627c368ae4ac514d33538e0.

--- dashboard/components/alert_feed.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-def render_alert_feed():
-    st.markdown("""
-        <div data-testid="stMetric" style="margin-bottom: 1rem; text-align: center; color: #666; font-weight: 500;">
-            No active high-severity anomalies at this time.
-        </div>
-    """, unsafe_allow_html=True)
