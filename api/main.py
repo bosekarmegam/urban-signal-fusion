b2eef6b9e6d0b39dbc8dbd6e2f0154c84e08745f
bosekarmegam <suneelbosekarmegam@gmail.com>
2026-03-03 11:38:18 +0530

    update

--- api/main.py
+++ api/main.py
@@ -19,7 +19,7 @@ app = FastAPI(title=settings.project_name, lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
